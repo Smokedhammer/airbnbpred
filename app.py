fa193c769a21e6aed84da9fd8faf70e2c6096f6c
unknown <sohamagarwal00@gmail.com>
2023-12-06 18:38:10 -0500

    master

--- app.py
+++ app.py
@@ -45,20 +45,39 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 data ="Optimization terminated successfully (Exit mode 0) Current function value: 0.14186434517999363 Iterations: 186 Function evaluations: 187 Gradient evaluations: 186 Logit Regression Results Dep. Variable: Superhost No. Observations: 40155 Model: Logit Df Residuals: 40144 Method: MLE Df Model: 10 Date: Tue, 05 Dec 2023 Pseudo R-squ.: 0.7883 Time: 15:42:28 Log-Likelihood: -5676.7 converged: True LL-Null: -26817. Covariance Type: nonrobust LLR p-value: 0.000 Coefficients: const: -1.7599 (std err: 0.209, z: -8.429, P>|z|: 0.000, [0.025-0.975]: [-2.169, -1.351]) hostResponseAverage_pastYear: 0.6660 (std err: 0.036, z: 18.681, P>|z|: 0.000, [0.025-0.975]: [0.596, 0.736]) tract_superhosts_ratio: 1.0328 (std err: 0.033, z: 31.446, P>|z|: 0.000, [0.025-0.975]: [0.968, 1.097]) prev_rating_ave_pastYear: 2.2915 (std err: 0.087, z: 26.376, P>|z|: 0.000, [0.025-0.975]: [2.121, 2.462]) superhost_change_gain_superhost: 5.4226 (std err: 0.686, z: 7.899, P>|z|: 0.000, [0.025-0.975]: [4.077, 6.768])  num_5_star_Rev_pastYear: 2.3450 (std err: 0.090, z: 26.195, P>|z|: 0.000, [0.025-0.975]: [2.170, 2.520]) rating_ave_pastYear: 1.8790 (std err: 0.089, z: 21.041, P>|z|: 0.000, [0.025-0.975]: [1.704, 2.054]) prev_numCancel_pastYear: -1.5190 (std err: 0.062, z: -24.521, P>|z|: 0.000, [0.025-0.975]: [-1.640, -1.398]) numCancel_pastYear: -1.1112 (std err: 0.060, z: -18.660, P>|z|: 0.000, [0.025-0.975]: [-1.228, -0.994]) Number of Reviews: 0.0782 (std err: 0.030, z: 2.600, P>|z|: 0.009, [0.025-0.975]: [0.019, 0.137]) prev_num_5_star_Rev_pastYear: 1.7979 (std err: 0.079, z: 22.623, P>|z|: 0.000, [0.025-0.975]: [1.642, 1.954]) Possibly complete quasi-separation: A fraction 0.23 of observations can be perfectly predicted. This might indicate that there is complete quasi-separation. In this case, some parameters will not be identified."
 
 
-def bug_fixer(text_input):
-  response = openai.Completion.create(
-  engine="text-davinci-003",
-  messages=[
-      {"role": "user", "content": "Tell the following in a way that is explainable to clients, dont talk technical. Based on only the following: answer the following"+data+"Tell the user the how much it matters and implications of the answer as well"+fixed}
-            
-   ]
-  
-)
+#def bug_fixer(text_input):
+#  response = openai.Completion.create(
+#  engine="text-davinci-003",
+#  messages=[
+#      {"role": "user", "content": "Tell the following in a way that is explainable to clients, dont talk technical. Based on only the following: answer the following"+data+"Tell the user the how much it matters and implications of the answer as well"+fixed}
+#            
+#   ]
+#  
+#)
 
+import openai
 
-  
-  
-  return response['choices'][0]['message']['content']
+import openai
+
+def bug_fixer(text_input, data, fixed, api_key):
+    try:
+        # Set the API key
+        openai.api_key = api_key
+
+        # Create the message
+        prompt_message = "Tell the following in a way that is explainable to clients, don't talk technical. Based on only the following: answer the following " + data + " Tell the user how much it matters and the implications of the answer as well " + fixed
+        
+        # Call the OpenAI API
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt_message,
+            max_tokens=150  # Adjust this value based on how long you want the response to be
+        )
+        # Return the text part of the response
+        return response.choices[0].text.strip()
+    except Exception as e:
+        # Handle any errors that occur during the API call
+        return f"An error occurred: {str(e)}"
 
 
 # Now we create the frontend code and all
