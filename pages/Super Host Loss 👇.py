c528f0248e7dd8c77d7265ed3e690ec055198ab3
unknown <sohamagarwal00@gmail.com>
2023-12-07 16:58:25 -0500

    master

--- pages/Super Host Loss 👇.py
+++ pages/Super Host Loss 👇.py
@@ -69,7 +69,7 @@ def bug_fixer(data, fixed, api_key):
         # Create the messages for the chat completion
         messages = [
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": data + " Knowing the data, answer the following question to the user in a realistic, simple way. Use the data to support the answers. " + fixed + " . Interpret the coefficients properly considering it is in log log model and answer the users properly. Tell the user how much it matters and the implications of the answer as well. If the question is not relevant to context, tell them it's not a relevant question."}
+            {"role": "user", "content": data + " Knowing the data, answer the following question to the user in a realistic, simple way. Use the data to support the answers. " + fixed + " Convert the coefficents using: round((math.exp(coef*0.01) -1) * 100 , 3). Interpret the coefficients properly and answer the users properly. Tell the user how much it matters and the implications of the answer as well. If the question is not relevant to context, tell them it's not a relevant question."}
         ]
 
         # Call the OpenAI API using the chat completions endpoint
